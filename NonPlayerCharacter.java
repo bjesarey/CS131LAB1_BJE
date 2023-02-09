aec0274ea760b5929e7d430a14fdf0f50115093b
bjesarey <122053831+bjesarey@users.noreply.github.com>
2023-02-08 20:56:24 -0500

    Add files via upload

---
+++ NonPlayerCharacter.java
@@ -0,0 +1,39 @@
+//import java.security.SecureRandom;
+
+/**
+ * This is our NonPlayer Character that inherits from GameCharacter it represent
+ * objects the game controls (as opposed to objects a human player controls).
+ * 
+ * @author  Rob Kelley, but modified by Brandon Esarey
+ * @version 1.3 Lab1 CS131ON
+ * NonPlayerCharacter
+ * Semester year 2023
+ */
+
+public class NonPlayerCharacter extends GameCharacter {
+	private boolean Behavior;
+	private String IntelligenceType;
+
+	public void setBehavior(boolean active) {
+		Behavior = active;
+	}
+
+	public boolean getBehavior() {
+		return Behavior;
+	}
+
+	public void setIntelligenceType(String intelligence) {
+		IntelligenceType = intelligence;
+	}
+
+	public String getIntelligenceType() {
+		return IntelligenceType;
+	}
+
+	public NonPlayerCharacter() {
+		super();
+		setBehavior(false);
+		setIntelligenceType("AVERAGE");
+	}// End empty argument constructor.
+
+}// end class
